d1ab870b59e0c26c548e1004049c303affcbb5c5
kaltriumsystems-hue <kaltrium.systems@gmail.com>
2025-10-31 13:55:39 +0100

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,4 @@
+// /api/ping.js
 export default async function handler(req, res) {
-  res.status(200).json({ ok: true, message: "pong" });
+  res.status(200).json({ ok: true, message: 'pong', ts: Date.now() });
 }
